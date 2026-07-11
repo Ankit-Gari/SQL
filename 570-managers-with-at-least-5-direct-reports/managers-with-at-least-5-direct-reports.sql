2d57acbc027a26afd9fc4bf815057d41ba137d2b
Ankit Gari <83578095+Ankit-Gari@users.noreply.github.com>
2026-07-12 01:25:24 +0530

    Time: 401 ms (9.68%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,12 +1,5 @@
 -- Write your PostgreSQL query statement below
 
--- select e1.id, e1.name, e2.managerid, count()
--- select *
--- from Employee e1
--- left join Employee e2
--- on e1.managerId = e2.id
--- and e1.managerId = e2.managerId
-
 select E1.name
 from Employee E1
 join

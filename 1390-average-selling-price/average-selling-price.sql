04c71a737f5755bc426c483177a2f426da3dbba4
Ankit Gari <83578095+Ankit-Gari@users.noreply.github.com>
2026-07-16 21:42:17 +0530

    Time: 251 ms (16.33%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -19,11 +19,7 @@
 
 
 select p.product_id,
-    case
-        when sum(u.units) is NULL
-        then 0
-        else round(sum(p.price * u.units)/sum(u.units)::numeric,2)
-    end as average_price
+    coalesce(round(sum(p.price * u.units)::numeric/sum(u.units), 2),0) as average_price
 from Prices p
 left join UnitsSold u
 on p.product_id = u.product_id and (u.purchase_date between p.start_date and p.end_date)

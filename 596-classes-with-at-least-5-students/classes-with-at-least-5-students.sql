23a2ac59e8d665827b496421d2b44c2b9e64d36c
Ankit Gari <83578095+Ankit-Gari@users.noreply.github.com>
2026-07-17 15:54:06 +0530

    Time: 284 ms (61.68%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,12 +1,19 @@
 -- Write your PostgreSQL query statement below
 
-select
-    class
-from(
-    select 
-        class,
-        count(*) class_count
-    from Courses
-    group by class
-)
-where class_count >= 5;
+select class
+from Courses
+group by class
+having count(*)>=5
+
+
+
+-- select
+--     class
+-- from(
+--     select 
+--         class,
+--         count(*) class_count
+--     from Courses
+--     group by class
+-- )
+-- where class_count >= 5;

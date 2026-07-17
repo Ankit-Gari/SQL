ce7b148dcaf526f5a3ed854c890f71ac2741d73b
Ankit Gari <83578095+Ankit-Gari@users.noreply.github.com>
2026-07-17 13:00:37 +0530

    Time: 615 ms (16.58%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -3,10 +3,10 @@
 with login_data as (
     select *, 
     lead(event_date) over(partition by player_id order by event_date) as next_day,
-    row_number() over(partition by player_id order by event_date) as played_continuously
+    row_number() over(partition by player_id order by event_date) as login_order
     from Activity
 )
 
 select
-    round(sum(case when next_day - event_date = 1 AND played_continuously = 1 then 1 else 0 end)*1.0/ count(distinct player_id), 2)as fraction 
+    round(sum(case when next_day - event_date = 1 AND login_order = 1 then 1 else 0 end)*1.0/ count(distinct player_id), 2)as fraction 
 from login_data
